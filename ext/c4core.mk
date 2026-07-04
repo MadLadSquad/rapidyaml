dac15747ca028fc38d4f41e7c3a828b90aefd5b7
Joao Paulo Magalhaes <dev@jpmag.me>
2026-07-04 01:30:13 +0100

    Update c4core

--- ext/c4core.mk
+++ ext/c4core.mk
@@ -3,4 +3,4 @@
 C4CORE_REPO := https://github.com/biojppm/c4core
 # must be either tag or a FULL commit hash; cannot
 # be a branch name or short commit hash!
-C4CORE_TAG := 9dbd9de742dde82d3faaecd36238ebeebdbd94b0
+C4CORE_TAG := a9ee249b261ef5734883e33f387542be9a829809
